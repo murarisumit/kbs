79ceefeff198e1a5d7b136738674521edf33f44c
sumit murari <murari.sumit@outlook.com>
2024-04-14 18:58:31 +0530

    Last time update to with local changes, bad

--- js/utils/darkmode.js
+++ js/utils/darkmode.js
@@ -1,29 +1,60 @@
-var toggle = document.getElementById("dark-mode-toggle");
+// var toggle = document.getElementById("dark-mode-toggle");
+// var darkTheme = document.getElementById("dark-mode-theme");
+
+var colorModeOverride = document.getElementById("color-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
 
-toggle.addEventListener("click", () => {
-    if (toggle.className === "fal fa-moon-o") {
-        setTheme("dark");
-    } else if (toggle.className === "fal fa-sun-o") {
-        setTheme("light");
+// set enum of color modes
+const ColorMode = {
+    DARK: "dark",
+    LIGHT: "light"   
+};
+
+const Cookies = {
+    COLOR_MODE_OVERRIDE: "color-mode-override"
+}
+
+const override_duration = 21600; // 6 hours
+
+// manually set the theme
+colorModeOverride.addEventListener("click", () => {
+    if (colorModeOverride.className === "fal fa-moon-o") {
+        document.cookie = `${Cookies.COLOR_MODE_OVERRIDE}=${ColorMode.DARK}; SameSite=Strict; max-age=${override_duration}; path=/`;
+        setTheme(ColorMode.DARK);
+    } else if (colorModeOverride.className === "fal fa-sun-o") {
+        document.cookie = `${Cookies.COLOR_MODE_OVERRIDE}=${ColorMode.LIGHT}; SameSite=Strict; max-age=${override_duration}; path=/`;
+        setTheme(ColorMode.LIGHT);
     }
 });
 
-// auto detect user's system theme
-// if saved theme is dark, set dark theme
-// else if saved theme is light, set light theme
-// else if system theme is dark, set dark theme
-if (localStorage.getItem("dark-mode-storage") === "dark") {
-    setTheme("dark");
-} else if (localStorage.getItem("dark-mode-storage") === "light") {
-    setTheme("light");
+// check if the user has set a color mode override earlier else check for the system preference
+var cookie_color_mode_override = document.cookie.split('; ').find(row => row.startsWith(`${Cookies.COLOR_MODE_OVERRIDE}`));
+if (cookie_color_mode_override) {
+    cookie_color_mode_override = cookie_color_mode_override.split('=')[1];
+    console.log(`Overriden: ${cookie_color_mode_override}`);
+    setTheme(cookie_color_mode_override);
 } else {
     const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    console.log(darkQuery);
     if (darkQuery.matches) {
-        setTheme("dark");
+        setTheme(ColorMode.DARK);
     }
 }
+
+// localStorage.setItem("dark-mode-storage", "dark");
+// localStorage.getItem("dark-mode-storage");
+// localStorage.removeItem("dark-mode-storage");
+
+// if (localStorage.getItem("dark-mode-storage") === "dark") {
+//     setTheme("dark");
+// } else if (localStorage.getItem("dark-mode-storage") === "light") {
+//     setTheme("light");
+// } else {
+//     const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+//     console.log(darkQuery);
+//     if (darkQuery.matches) {
+//         setTheme("dark");
+//     }
+// }
 
 // if (darkQuery.matches) {
 //     setTheme("dark");
@@ -37,13 +68,12 @@ if (localStorage.getItem("dark-mode-storage") === "dark") {
 
 
 function setTheme(mode) {
-    localStorage.setItem("dark-mode-storage", mode);
-    if (mode === "dark") {
+    if (mode === ColorMode.DARK) {
         darkTheme.disabled = false;
-        toggle.className = "fal fa-sun-o";
-    } else if (mode === "light") {
+        colorModeOverride.className = "fal fa-sun-o";
+    } else if (mode === ColorMode.LIGHT) {
         darkTheme.disabled = true;
-        toggle.className = "fal fa-moon-o";
+        colorModeOverride.className = "fal fa-moon-o";
     }
 }
 
